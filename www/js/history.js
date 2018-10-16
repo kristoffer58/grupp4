1845c9bc74be8c9346d02452c913d6b2db198acb
Unknown <melsie78@hotmail.com>
2018-10-16 22:48:52 +0200

    shortened text
    some js changes

--- www/js/history.js
+++ www/js/history.js
@@ -1,43 +1,53 @@
 $.getJSON('/json/history.json', start);
 
-function start(historyTrans){
+function start(historyTrans) {
 
-let ul = $('<ul class="historySv"/>');
-for(let explanation in historyTrans){
-    let li = $('<li/>');
-    for(let langSv of historyTrans[explanation].sv){
-        li.append('<p>' + langSv.desc + '</p>');
-    }
-    
+  let ul = $('<ul class="historySv"/>');
+
+  for (let explanation in historyTrans) {
+
+   
+    for (let langSv of historyTrans[explanation].sv) {
+      let li = $('<li/>');
+      li.append('<p>' + langSv.desc + '</p>');
+      ul.append(li);
     $('main').append(ul);
-
-    // english instructions
-    ul = $('<ul class="historyEn"/>');
-for(let explanation in historyTrans){
-    let li = $('<li/>');
-    for(let langEn of historyTrans[explanation].en){
-        li.append('<p>' + langEn.desc + '</p>');
     }
-    ul.append(li);
+  }
+
+  // english instructions
+  ul = $('<ul class="historyEn"/>');
+
+  for (let explanation in historyTrans) {
+
+    
+    for (let langEn of historyTrans[explanation].en) {
+      let li = $('<li/>');
+      li.append('<p>' + langEn.desc + '</p>');
+
+      ul.append(li);
+    }
   }
   // jQuery grab the body element
   // and append the ul inside it
   $('main').append(ul);
 }
-}
 
+$('historySv').hide();
 $('#flagSv').hide();
 
-$('#flagSv').click(function(){
+
+$('#flagSv').click(function () {
   $('#flagEn').show();
-  $('.instrEn').hide();
-  $('.instrSv').show();
+  $('.historyEn').hide();
+  $('.historySv').show();
   $('#flagSv').hide();
 });
 
-$('#flagEn').click(function(){
-  $('#flagSv').show();
-  $('.instrSv').hide();
-  $('.instrEn').show();
+$('#flagEn').click(function () {
   $('#flagEn').hide();
+  $('.historyEn').show();
+  $('.historySv').hide();
+  $('#flagSv').show();
 });
+
