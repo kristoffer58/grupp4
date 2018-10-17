8c587e2c71a9f4ff97fce91c22caaebd8fea4690
Unknown <melsie78@hotmail.com>
2018-10-17 10:49:45 +0200

    json and js and scss changes

--- www/js/history.js
+++ www/js/history.js
@@ -1,17 +1,22 @@
 $.getJSON('/json/history.json', start);
 
+let translateSv = {
+  "A brief history of Breakout": 'En kort historia i Breakout',
+}
+
 function start(historyTrans) {
 
   let ul = $('<ul class="historySv"/>');
 
   for (let explanation in historyTrans) {
+    let li = $('<li/>');
 
-   
     for (let langSv of historyTrans[explanation].sv) {
-      let li = $('<li/>');
-      li.append('<p>' + langSv.desc + '</p>');
+      
+      li.append('<p>' + langSv + '</p>');
       ul.append(li);
-    $('main').append(ul);
+    
+      $('main').append(ul);
     }
   }
 
@@ -19,21 +24,22 @@ function start(historyTrans) {
   ul = $('<ul class="historyEn"/>');
 
   for (let explanation in historyTrans) {
-
-    
+    li = $('<li/>');
     for (let langEn of historyTrans[explanation].en) {
-      let li = $('<li/>');
-      li.append('<p>' + langEn.desc + '</p>');
+     
+      li.append('<p>' + langEn + '</p>');
 
       ul.append(li);
+    
+
+    // jQuery grab the body element
+    // and append the ul inside it
+    $('main').append(ul);
     }
   }
-  // jQuery grab the body element
-  // and append the ul inside it
-  $('main').append(ul);
 }
 
-$('historySv').hide();
+
 $('#flagSv').hide();
 
 
