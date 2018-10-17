930e5ec130543c30fdb674590f334055c23f9843
Kim <kim.johnsson@telia.com>
2018-10-17 09:29:59 +0200

    Translation

---
+++ www/js/tryMeButton.js
@@ -0,0 +1,35 @@
+function start(button){
+
+    let ul = $('<ul class = "langSv"/>');
+    for(let tryGame in button){
+        let li = $('<li/>');
+        for(let langSv of button[tryGame].sv){
+            li.append('<p>' + langSv.name + '</p>');
+        }
+        ul.append(li);
+        $('main .card-body a').append(ul);
+    
+        ul = $('<ul class = "langEn"/>');
+        for(let tryGame in button){
+          let li = $('<li/>');
+          for(let langEn of button[tryGame].en){
+              li.append('<p>' + langEn.name + '</p>');
+          }
+        ul.append(li);
+      }
+    
+      $('main .card-body a').append(ul);
+    }
+}
+$.getJSON('/json/tryMeButton.json', start);
+
+
+$('.engelska').click(function(){
+    $('.langSv').hide();
+    $('.langEn').show();
+});
+
+$('.sverige').click(function(){
+    $('.langEn').hide();
+    $('.langSv').show();
+});
