fc34b0e6639d79ddfb2ad3a8e7e3ef0530fd6f6e
Kim <kim.johnsson@telia.com>
2018-10-17 11:11:34 +0200

    s

--- www/js/tryMeButton.js
+++ www/js/tryMeButton.js
@@ -1,25 +1,55 @@
 function start(button){
 
-    let ul = $('<ul class = "langSv"/>');
+    //text
+    //sedish
+    let ulText = $('<ul class = "langSv"/>');
+
+    for(let tryGame in button){
+        let li = $('<li/>');
+        for(let langSv of button[tryGame].sv){
+            li.append('<h5>' + langSv.head + '</h5>');
+        }
+        ulText.append(li);
+    }
+    $('main .tryIt').append(ulText);
+
+    //english
+    ulText = $('<ul class = "langEn"/>');
+
+    for(let tryGame in button){
+        let li = $('<li/>');
+        for(let langEn of button[tryGame].en){
+            li.append('<h5>' + langEn.head + '</h5>');
+        }
+        ulText.append(li);
+    }
+    $('main .tryIt').append(ulText);
+
+
+    //buttons
+    //Swedish
+    let ulButton = $('<ul class = "langSv"/>');
     for(let tryGame in button){
         let li = $('<li/>');
         for(let langSv of button[tryGame].sv){
             li.append('<p>' + langSv.name + '</p>');
         }
-        ul.append(li);
-        $('main .card-body a').append(ul);
-    
-        ul = $('<ul class = "langEn"/>');
-        for(let tryGame in button){
-          let li = $('<li/>');
-          for(let langEn of button[tryGame].en){
-              li.append('<p>' + langEn.name + '</p>');
-          }
-        ul.append(li);
-      }
-    
-      $('main .card-body a').append(ul);
+        ulButton.append(li);
     }
+    $('main .card-body a').append(ulButton);
+
+
+    //English
+    ulButton = $('<ul class = "langEn"/>');
+    for(let tryGame in button){
+        let li = $('<li/>');
+        for(let langEn of button[tryGame].en){
+            li.append('<p>' + langEn.name + '</p>');
+        }
+        ulButton.append(li);
+    }
+    $('main .card-body a').append(ulButton);
+    
 }
 $.getJSON('/json/tryMeButton.json', start);
 
@@ -27,9 +57,13 @@ $.getJSON('/json/tryMeButton.json', start);
 $('.engelska').click(function(){
     $('.langSv').hide();
     $('.langEn').show();
+    $('.sverige').show();
+    $('.engelska').hide();
 });
 
 $('.sverige').click(function(){
     $('.langEn').hide();
     $('.langSv').show();
+    $('.engelska').show();
+    $('.sverige').hide();
 });
