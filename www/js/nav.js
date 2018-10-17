231a07c92977402f5e7140edbd90954c89c0b627
Mattias Hagert <matte.hagert@gmail.com>
2018-10-17 10:38:53 +0200

    Translation nav & footer

---
+++ www/js/nav.js
@@ -0,0 +1,27 @@
+$.getJSON('/json/nav.json', function (navData) {
+    start(navData, true);
+  });
+  let navMenu = '';
+  
+  function start(navData, inSwedish) {
+    navMenu = navData;
+    let languageObject = navData.en;
+    if (inSwedish == true) {
+      languageObject = navData.sv
+    }
+    for (let className in languageObject) {
+      $('.'+className).text(languageObject[className])
+    }
+  }
+  $('.engelska').on('click', function () {
+    start(navMenu, false);
+  });
+  $('.sverige').on('click', function () {
+    start(navMenu, true);
+  });
+  
+  // Class active follow the click on links 
+  $(".nav-link").on("click", function () {
+    $("a.nav-link").removeClass("active");
+    $(this).addClass("active");
+  });
