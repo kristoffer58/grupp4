0d3328f25aab4069687ee08ab0824ddd77399377
Unknown <melsie78@hotmail.com>
2018-10-18 11:34:17 +0200

    uppdate style.scss

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('www'));
 // Start the web server on port ...
 const port=3000;
-console.log(require('os').networkInterfaces());
+//console.log(require('os').networkInterfaces());
 
 app.listen(port,() => console.log('Listening on port '+port));
 const Sass = require('./sass');
