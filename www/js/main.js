5bcc2fb4732faf4a006568b1e082d9d421c870d5
lukasz kurasinski <lukasz.kurasinski@gmail.com>
2018-10-22 12:10:33 +0200

    active link in navbar change

--- www/js/main.js
+++ www/js/main.js
@@ -22,13 +22,43 @@ $(document).on('click', 'a', function(e){
   // The router (do whatever you want here)
   function frontendRouter(path){
     let routes = {
-      '/': () => { $('body main > *').hide(); $('.Start').show(); },
-      '/history': () => { $('body main > *').hide(); $('.Historia').show(); },
-      '/game': () => { $('body main > *').hide(); $('.Spela').show(); },
-      '/404': () => { $('body main > *').hide(); $('.the404').show(); },
+      '/': () => { $('body main > *').hide(); $('.Start').show();
+      //this will be optimized changes current site to active in navbar
+      $('.history').removeClass('active');
+      $('.play-game').removeClass('active');
+      $('.home').addClass('active');
+      changeActiveLink('/');
+    },
+      '/history': () => { $('body main > *').hide(); $('.Historia').show();
+      //this will be optimized changes current site to active in navbar
+      $('.home').removeClass('active');
+      $('.play-game').removeClass('active');
+      $('.history').addClass('active');
+      changeActiveLink('/');
+    },
+      '/game': () => { $('body main > *').hide(); $('.Spela').show(); 
+      //this will be optimized changes current site to active in navbar
+      $('.home').removeClass('active');
+      $('.history').removeClass('active');
+      $('.play-game').addClass('active');
+      changeActiveLink('/');
+    },
+      '/404': () => { $('body main > *').hide(); $('.the404').show(); 
+      
+    },
     };
     // no path found then change path to '/404';
     path = routes[path] ? path : '/404';
     // run the function associated with the path
     routes[path]();
+  }
+
+
+  //Still working on it Lukas
+  // optymized ver of active class change in nav bar 
+  function changeActiveLink(activeLink){
+
+   let links = $("a").toArray();
+   console.log(links.toString());
+   
   }
