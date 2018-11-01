05e38bc05e94dbe396021e92f01aef1600a51d8a
melli <melsie78@hotmail.com>
2018-11-01 10:02:35 +0100

    changes and updates

--- www/js/modal.js
+++ www/js/modal.js
@@ -1,6 +1,6 @@
-$("#submit-highscore button").click(function() {
+$("#submit-highscore button").click(function () {
   let fname = $("input[name='fname']").val();
-  $.post( "/add-score", { "name": fname, "score": score || 0 }, function(responseData) {
+  $.post("/add-score", { "name": fname, "score": score || 0 }, function (responseData) {
     console.log('the new highscore-list is:', responseData);
     console.error('append/use the new highscore-list then remove this console.error');
   });
