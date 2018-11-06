acf5d3a6e0abdd2b6915bf6fff649e912768bf14
melli <melsie78@hotmail.com>
2018-11-06 13:50:22 +0100

    cards lightbox modal js, html, css

--- www/js/cards-lightbox.js
+++ www/js/cards-lightbox.js
@@ -1,3 +1,8 @@
 $(".Start .card img").on('click', function(){
+  
   $('#cardsModal img').attr('src', $(this).attr('src'));
-})
+});
+
+$("#cardsModal").on('click', function(){
+  $("#cardsModal").hide();
+});
